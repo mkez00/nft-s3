da98fb398699453132f51b4e46ae7e0f01910265
Michael Kezele <mikekezele@gmail.com>
2022-02-22 19:25:26 -0500

    include provider in config

--- backend/nfts3-fetch-config/config.py
+++ backend/nfts3-fetch-config/config.py
@@ -5,10 +5,12 @@ def lambda_handler(event, context):
     
     account = os.environ['account']
     value = os.environ['value']
+    blockchain_provider = os.environ['blockchain_provider']
     
     response = {
         "account": account,
-        "value": value
+        "value": value,
+        "blockchain_provider": blockchain_provider
     }
     
     return response
